68d2631d80a2242bbf36dc39e915e11d77019045
YanMachadoCarrillo <yanca.next@gmail.com>
2026-05-03 16:40:15 -0500

    Update server.js

--- mysql/server.js
+++ mysql/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fetch = global.fetch;
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const PORT = process.env.PORT || 3000;
